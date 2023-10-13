f3eedd64f917efccbe06d93fee731d1fe7bd784f
Jaba <jabaqadagishvili4@gmail.com>
2023-10-13 13:27:34 +0400

    Algorithms were developed

--- src/gameAlgorithm.js
+++ src/gameAlgorithm.js
@@ -1,4 +1,4 @@
-
+const squareRegistry = new Map()
 
 const chessSquare = (x, y) => {
     const xPosition = x
@@ -33,7 +33,46 @@ const chessSquare = (x, y) => {
             return chessSquare(newXPosition, newYPosition)
         }
     }
+
+    if (squareRegistry.has(coordName())) {
+        return squareRegistry.get(coordName())
+    } else {
+        const newSquare = { coordName, getPredecessor, setPredecessor, possibleMoves }
+        squareRegistry.set(coordName(), newSquare)
+        return newSquare
+    }
+}
+
+const knightTravails = (start, finish) => {
+    squareRegistry.clear()
+
+    const origin = chessSquare(...start)
+    const target = chessSquare(...finish)
+
+    const queue = [origin]
+
+    while (!queue.includes(target)) {
+        const currentSquare = queue.shift();
+
+        const enqueueList = currentSquare.possibleKnightMoves();
+        enqueueList.forEach((square) => square.setPredecessor(currentSquare));
+        queue.push(...enqueueList);
+    }
+    const path = [target]
+    while (!path.includes(origin)) {
+        const prevSquare = path[0].getPredecessor();
+        path.unshift(prevSquare);
+    }
+    let squareCoord = [];
+    path.forEach((square) => {
+        // console.log(square.name());
+        squareCoord.push(square.name());
+    });
+    // console.log(squareCoord);
+    showMoves(path, squareCoord);       //Not added yet
+
 }
 
 
-export { chessSquare }
+
+export { knightTravails }
